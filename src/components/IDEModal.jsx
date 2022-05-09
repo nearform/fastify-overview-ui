c71d46165b783c2f7da63514738731a0a47da151
Raúl Sáez <raulsaezp@gmail.com>
2022-05-09 17:10:37 +0100

    wip

---
+++ src/components/IDEModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function IDEModal () {
+  return (
+    <div className='left-0 top-0 fixed z-10 w-full h-full'>
+        <div className='w-80 h-40 translateY-half m-auto top-half left-0 right-0 absolute shadow bg-gray-50 rounded-lg p-4'>
+            Modal content
+        </div>
+    </div>
+  )
+}
